4ddec6037a7f551fc65beaa0776d511e63009811
Hamza Ayoub <infohamza2@gmail.com>
2024-11-20 23:55:50 +0500

    Change PORT setting

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import cv2
 
 app = Dash(__name__)
 
@@ -170,4 +171,4 @@ def update_customer_chart(_):
     return fig
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8050, debug=True)
